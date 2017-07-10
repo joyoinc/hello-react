30944f6020eddbce413221284668f7258b70a976
yzhi <yzhi@connectwise.com>
2017-07-10 13:19:02 -0400

    init proj

---
+++ client/component/app.jsx
@@ -0,0 +1,15 @@
+/*
+ *  ./client/component/add.jsx
+ * */
+
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return ( <div>
+        <h1>Hello </h1>
+        <h2> this is a test web app </h2>
+      </div>
+    );
+  }
+}
---
+++ client/index.js
@@ -0,0 +1,11 @@
+/*
+ *  ./client/index.js
+ * */
+
+console.log('hello guys!')
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './component/app.jsx'
+
+ReactDOM.render(<App />, document.getElementById('root'));
